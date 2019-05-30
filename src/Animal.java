bc924dcc1a6ecf23400ecfebf2bc04209f9e9b97
Gabor <orbangabeszb@gmail.com>
2019-05-30 20:19:01 +0200

    continents HashSet initialized

--- src/Animal.java
+++ src/Animal.java
@@ -1,24 +1,16 @@
+import java.util.HashSet;
+import java.util.Set;
 
 public class Animal {
 	private final String name;
 	private final Origin origin;
 	private final Diet diet;
 	
-	//Origin continents initialized
-	private static final Origin EUROPE = new Origin("Europe", "Europe is a continent located entirely in the Northern Hemisphere\n"
+	Origin europe = new Origin("Europe", "Europe is a continent located entirely in the Northern Hemisphere\n"
 			+ " and mostly in the Eastern Hemisphere.");
-	private static final Origin ASIA = new Origin("Asia", "Asia is Earth's largest and most populous continent, located primarily\n"
-			+ " in the Eastern and Northern Hemispheres.");
-	private static final Origin AFRICA = new Origin("Africa", "Africa is the world's second largest and second most-populous continent,\n"
-			+ " being behind Asia in both categories.");
-	private static final Origin NORTH_AMERICA = new Origin("North America", "North America is a continent entirely within the Northern Hemisphere\n"
-			+ " and almost all within the Western Hemisphere.");
-	private static final Origin SOUTH_AMERICA = new Origin("South-America", "South America is a continent in the Western Hemisphere, mostly in the Southern Hemisphere,\n"
-			+ "with a relatively small portion in the Northern Hemisphere.");
-	private static final Origin AUSTRALIA = new Origin("Australia", "The continent of Australia, sometimes known in technical contexts by the names Sahul, Australinea or Meganesia\n"
-			+ " to distinguish it from the country of Australia, consists of the land masses\n"
-			+ " which sit on Australia's continental plate.");
-				
+
+	private final Set<Origin> continents = new HashSet<Origin>();
+
 	public Animal(String name, Origin origin, Diet diet) {
 		this.name = name;
 		this.origin = origin;
@@ -36,7 +28,12 @@ public class Animal {
 	public Diet getDiet() {
 		return diet;
 	}
-	
+
+	public Set<Origin> getContinents() {
+		return continents;
+	}
+
+
 	private class Origin {
 		private final String continentName;
 		private final String continentDescription;
@@ -45,6 +42,21 @@ public class Animal {
 			this.continentName = continentName;
 			this.continentDescription = continentDescription;
 		}
+		
+		//Origin continents initialized
+		private final Origin europe = new Origin("Europe", "Europe is a continent located entirely in the Northern Hemisphere\n"
+				+ " and mostly in the Eastern Hemisphere.");
+		private final Origin asia = new Origin("Asia", "Asia is Earth's largest and most populous continent, located primarily\n"
+				+ " in the Eastern and Northern Hemispheres.");
+		private  final Origin africa = new Origin("Africa", "Africa is the world's second largest and second most-populous continent,\n"
+				+ " being behind Asia in both categories.");
+		private final Origin northAmerica = new Origin("North America", "North America is a continent entirely within the Northern Hemisphere\n"
+				+ " and almost all within the Western Hemisphere.");
+		private final Origin southAmerica = new Origin("South-America", "South America is a continent in the Western Hemisphere, mostly in the Southern Hemisphere,\n"
+				+ "with a relatively small portion in the Northern Hemisphere.");
+		private final Origin australia = new Origin("Australia", "The continent of Australia, sometimes known in technical contexts by the names Sahul, Australinea or Meganesia\n"
+				+ " to distinguish it from the country of Australia, consists of the land masses\n"
+				+ " which sit on Australia's continental plate.");
 
 		public String getContinentName() {
 			return continentName;
@@ -52,6 +64,16 @@ public class Animal {
 
 		public String getContinentDescription() {
 			return continentDescription;
+		}
+		
+		
+	}
+	
+	private class Diet {
+		private final String name;
+
+		public Diet(String name) {
+			this.name = name;
 		}
 		
 		
