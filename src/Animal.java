be2320689874ff1f897441ae235a2da4e89d1fb2
Gabor <orbangabeszb@gmail.com>
2019-05-30 20:49:44 +0200

    equals and hashcode overridden

--- src/Animal.java
+++ src/Animal.java
@@ -1,17 +1,10 @@
-import java.util.HashSet;
-import java.util.Set;
 
 public class Animal {
 	private final String name;
-	private final Origin origin;
-	private final Diet diet;
+	private final String origin;
+	private final String diet;
 	
-	Origin europe = new Origin("Europe", "Europe is a continent located entirely in the Northern Hemisphere\n"
-			+ " and mostly in the Eastern Hemisphere.");
-
-	private final Set<Origin> continents = new HashSet<Origin>();
-
-	public Animal(String name, Origin origin, Diet diet) {
+	public Animal(String name, String origin, String diet) {
 		this.name = name;
 		this.origin = origin;
 		this.diet = diet;
@@ -21,65 +14,34 @@ public class Animal {
 		return name;
 	}
 
-	public Origin getOrigin() {
+	public String getOrigin() {
 		return origin;
 	}
 
-	public Diet getDiet() {
+	public String getDiet() {
 		return diet;
 	}
 
-	public Set<Origin> getContinents() {
-		return continents;
+	@Override
+	public boolean equals(Object obj) {
+		if(this == obj) {
+			return true;
+		} else if(obj == null) {
+			return false;
+		} else if(obj instanceof Animal) {
+			Animal test = (Animal) obj;
+			if(test.getName() == this.getName()) {
+				return true;
+			}
+		}
+		return false;
 	}
 
-
-	private class Origin {
-		private final String continentName;
-		private final String continentDescription;
-
-		public Origin(String continentName, String continentDescription) {
-			this.continentName = continentName;
-			this.continentDescription = continentDescription;
-		}
-		
-		//Origin continents initialized
-		private final Origin europe = new Origin("Europe", "Europe is a continent located entirely in the Northern Hemisphere\n"
-				+ " and mostly in the Eastern Hemisphere.");
-		private final Origin asia = new Origin("Asia", "Asia is Earth's largest and most populous continent, located primarily\n"
-				+ " in the Eastern and Northern Hemispheres.");
-		private  final Origin africa = new Origin("Africa", "Africa is the world's second largest and second most-populous continent,\n"
-				+ " being behind Asia in both categories.");
-		private final Origin northAmerica = new Origin("North America", "North America is a continent entirely within the Northern Hemisphere\n"
-				+ " and almost all within the Western Hemisphere.");
-		private final Origin southAmerica = new Origin("South-America", "South America is a continent in the Western Hemisphere, mostly in the Southern Hemisphere,\n"
-				+ "with a relatively small portion in the Northern Hemisphere.");
-		private final Origin australia = new Origin("Australia", "The continent of Australia, sometimes known in technical contexts by the names Sahul, Australinea or Meganesia\n"
-				+ " to distinguish it from the country of Australia, consists of the land masses\n"
-				+ " which sit on Australia's continental plate.");
-
-		public String getContinentName() {
-			return continentName;
-		}
-
-		public String getContinentDescription() {
-			return continentDescription;
-		}
-		
-		
-	}
-	
-	private class Diet {
-		private final String name;
-
-		public Diet(String name) {
-			this.name = name;
-		}
-		
-		
+	@Override
+	public int hashCode() {
+		return this.name.hashCode() + 57 + this.origin.hashCode();
 	}
 	
 	
-	
-	
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,31 @@
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
 	
 	public static void main(String[] args) {
-		Animal elephant = new Animal("elephant", africa, )
+		Set<Animal> zoo = new HashSet<>();
+		
+		Animal elephant = new Animal("elephant", "Africa", "herbivore");
+		Animal wolf = new Animal("wolf", "Europe", "carnivore");
+		Animal kangoroo = new Animal("kangoroo", "Australia", "herbivore");
+		
+		zoo.add(elephant);
+		zoo.add(wolf);
+		zoo.add(kangoroo);
+		
+		printAnimals(zoo);
+		
+		Animal anotherWolf = new Animal("wolf", "Europe", "carnivore");
+		zoo.add(anotherWolf);
+		System.out.println();
+		
+		printAnimals(zoo);
+	}
+	
+	public static void printAnimals(Set<Animal> animals) {
+		for(Animal animal : animals) {
+			System.out.println(animal.getName() + ": lives in " + animal.getOrigin() + ", " + animal.getDiet());
+		}
 	}
 }
